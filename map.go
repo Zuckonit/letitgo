933b665a2b7da5194c0f2988f8b9629fe1ef00fa
Zuckonit <lsin30foxmail.com>
2015-03-06 10:23:39 +0800

    add map

---
+++ map.go
@@ -0,0 +1,24 @@
+// Package main provides ...
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    //define a map
+    m := make(map[string]int)
+    m["k1"] = 1
+    m["k2"] = 2 
+    fmt.Println("define then assign map: ", m)
+
+    //define and assign a map
+    m2 := map[string]int{"k1": 1, "k2": 2}
+    fmt.Println("define and assign map: ", m2)
+
+    //judge whether a key is in a map
+    _, prs := m["k2"]
+    fmt.Println("k2 in map: ", prs)
+    _, prs2 := m["k3"]
+    fmt.Println("k3 in map: ", prs2)
+}
